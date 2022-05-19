9e669e26ee6af3000ee4c2410428d9b415d24637
MinJeong Kim <ikwmjj@naver.com>
2022-05-19 12:23:10 +0900

    #2 클래스 사용
    // 배운 내용
    - c#에서는 list 객체 만들기 가능
    - for문보다 foreach문 사용하기~

--- CSClass3203/Program.cs
+++ CSClass3203/Program.cs
@@ -17,11 +17,26 @@ namespace Csclass_3203
             car.SetOutTime();
             // 메서드를 선언한 후, 오류나는 곳에 Alt+Enter -> 메서드 만들기 클릭 -> 함수 구현
 
-            // 랜덤 
+            
+            // 랜덤 클래스
             Random random = new Random();
             Console.WriteLine(random.Next(10, 100));
             Console.WriteLine(random.NextDouble());
-            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10+"\n");
+
+          
+            // 리스트 클래스
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(2);
+            list.Add(0);
+            list.Add(3);
+
+            foreach(int i in list) // foreach문을 사용하도록 하자
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
 
         }
     }
