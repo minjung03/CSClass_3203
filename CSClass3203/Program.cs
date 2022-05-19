342d432f2c5ce21942ccaff73f669733b179aa71
MinJeong Kim <ikwmjj@naver.com>
2022-05-19 12:10:27 +0900

    #2 클래스 사용
    // 배운내용
    - Ramdom 클래스 사용
    - F1 도움말 단축키 잘 사용하기!

--- CSClass3203/Program.cs
+++ CSClass3203/Program.cs
@@ -13,10 +13,16 @@ namespace Csclass_3203
             Car car = new Car();
             // 객체를 먼저 선언한 후, 오류나는 곳에 Alt+Enter -> 새 형식으로 class 만들기 클릭
 
-
             car.SetInTime();
             car.SetOutTime();
             // 메서드를 선언한 후, 오류나는 곳에 Alt+Enter -> 메서드 만들기 클릭 -> 함수 구현
+
+            // 랜덤 
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble() * 10);
+
         }
     }
 }
