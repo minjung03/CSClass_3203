3da4884af3a5a91016827a5b50bfb4ae239867f9
MinJeong Kim <ikwmjj@naver.com>
2022-05-19 11:46:27 +0900

    #1 클래스 생성 간단 예제
    // 배운 내용
    - C#의 자동완성 기능은 매우 편리하다
    - C#에서 메서드 선언 시 첫글자는 대문자로(스타일)

---
+++ CSClass3203/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csclass_3203
+{
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime() // 메서드 첫글자는 대문자 (C# 스타일 (Java는 소문자))
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+
+}
--- CSClass3203/Program.cs
+++ CSClass3203/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSClass3203
+namespace Csclass_3203
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            // 객체를 먼저 선언한 후, 오류나는 곳에 Alt+Enter -> 새 형식으로 class 만들기 클릭
+
+
+            car.SetInTime();
+            car.SetOutTime();
+            // 메서드를 선언한 후, 오류나는 곳에 Alt+Enter -> 메서드 만들기 클릭 -> 함수 구현
         }
     }
 }
