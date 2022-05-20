25a45713eaba47b61feabc37f684355099c8615d
MinJeong Kim <ikwmjj@naver.com>
2022-05-20 10:03:32 +0900

    #1 Xml 주석 달기
    // 배운내용
    - /// 로 코드에 대한 설명을 만들 수 있다!

--- CSClass3203/Car.cs
+++ CSClass3203/Car.cs
@@ -2,17 +2,26 @@
 
 namespace Csclass_3203
 {
+    /// <summary>
+    /// 자동차 주차장 입차, 출차 시간을 저장하는 클래스 입니다.
+    /// </summary>
     class Car
     {
         int carNumber;
         DateTime inTime;
         DateTime outTime;
-
+        
+        /// <summary>
+        /// 자동차 입차 시간을 지정합니다.
+        /// </summary>
         public void SetInTime() // 메서드 첫글자는 대문자 (C# 스타일 (Java는 소문자))
         {
             this.inTime = DateTime.Now;
         }
 
+        /// <summary>
+        /// 자동차 출차 시간을 지정합니다.
+        /// </summary>
         public void SetOutTime()
         {
             this.outTime = DateTime.Now;
